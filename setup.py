c49833fd8ceb82d67a9c8585d83e04367f9295f0
etienne <guichardetienne@gmail.com>
2024-11-18 14:08:55 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='GeneCA',
+version='0.1',
+description='Neural CA implementation for gene enabled CA.',
+author='Etienne Guichard',
+author_email='guichardetienne@gmail.com',
+classifiers=[
+    "Programming Language :: Python :: 3"],
+license='MIT',
+packages=["NCA"],
+zip_safe=False)
