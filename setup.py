4150dad8468cfe18b919d7695cbc448ffde157ce
etimush <83408933+etimush@users.noreply.github.com>
2025-04-29 12:50:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 setup(name='GeneCA',
 version='0.1',
 description='Neural CA implementation for gene enabled CA.',
-author='Etienne Guichard',
-author_email='guichardetienne@gmail.com',
+author='Anonymus',
+author_email='Anonymus@mail.com',
 classifiers=[
     "Programming Language :: Python :: 3"],
 license='MIT',
